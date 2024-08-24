5e916b4374f303a2e8aeb0282220860158be07cf
JulianDGP <juliandgp@hotmail.com>
2024-08-23 21:18:55 -0500

    configuracion de despliegue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'http://juliandgp.github.io/project-manager/' // This is the name of the repository
+  base: 'https://juliandgp.github.io/project-manager/' // This is the name of the repository
 })
